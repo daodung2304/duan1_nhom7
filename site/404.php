f037c856d2aaf9e144d5c56b5cd3725e53bafa24
daodung2304 <dungdaph50585@gmail.com>
2025-04-04 21:16:15 +0700

    add giao dien client

---
+++ site/404.php
@@ -0,0 +1,23 @@
+<main>
+   <!-- error area start -->
+   <section class="tp-error-area pt-110 pb-110">
+      <div class="container">
+         <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-8 col-md-10">
+               <div class="tp-error-content text-center">
+                  <div class="tp-error-thumb">
+                     <img src="assets/img/error/error.png" alt="">
+                  </div>
+
+                  <h3 class="tp-error-title">Page not found</h3>
+                  <p>Không tìm thấy trang</p>
+
+                  <a href="index.php" class="tp-error-btn">Back to Home</a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!-- error area end -->
+
+</main>
